910de4412b4440eaf0233b49ffdde9c96fec8540
margadrdn <emeralds0214@gmail.com>
2023-10-29 21:54:09 -0500

    basic post request implementation

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type post struct {
+	Id     int    `json:"id"`
+	Body   string `json:"body"`
+	UserId int    `json:"user-id"`
+}
+
+var posts = []post{
+	{Id: 1, Body: "Dolore aliquip ipsum ullamco enim Lorem pariatur fugiat eu fugiat.", UserId: 1},
+	{Id: 2, Body: "Sit duis laboris ut laboris fugiat enim est qui amet.", UserId: 1},
+	{Id: 3, Body: "Sint dolore mollit officia eu reprehenderit est minim.", UserId: 2},
+}
+
+func getPosts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, posts)
+}
+
 func main() {
-	fmt.Println("hello go")
+	router := gin.Default()
+	router.GET("/posts", getPosts)
+	router.Run("localhost:8080")
 }
